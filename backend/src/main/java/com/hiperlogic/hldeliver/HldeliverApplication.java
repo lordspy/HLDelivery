42e4bf613471a6b9f79c8cdbfd61f2b7b9497e7e
lordspy <lordspy@gmail.com>
2021-01-05 13:35:26 -0300

    project created

---
+++ backend/src/main/java/com/hiperlogic/hldeliver/HldeliverApplication.java
@@ -0,0 +1,13 @@
+package com.hiperlogic.hldeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HldeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HldeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hiperlogic/hldeliver/HldeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiperlogic.hldeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HldeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
