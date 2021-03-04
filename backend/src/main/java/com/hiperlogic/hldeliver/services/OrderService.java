004402f904d0b957b1c1aad93ef167597b9efb8a
lordspy <lordspy@gmail.com>
2021-03-03 23:57:55 -0300

    endpoint para criação de ordem

--- backend/src/main/java/com/hiperlogic/hldeliver/services/OrderService.java
+++ backend/src/main/java/com/hiperlogic/hldeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.hiperlogic.hldeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.hiperlogic.hldeliver.dto.OrderDTO;
+import com.hiperlogic.hldeliver.dto.ProductDTO;
 import com.hiperlogic.hldeliver.entities.Order;
+import com.hiperlogic.hldeliver.entities.OrderStatus;
+import com.hiperlogic.hldeliver.entities.Product;
 import com.hiperlogic.hldeliver.repositories.OrderRepository;
+import com.hiperlogic.hldeliver.repositories.ProductRepository;
 
 @Service
 public class OrderService {
@@ -17,10 +22,28 @@ public class OrderService {
 	@Autowired
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Transactional(readOnly = true)
 	public List<OrderDTO> findAll() {
 		List<Order> list = repository.findAllPendingLaterToNewer();
-		return list.stream().map(p -> new OrderDTO(p)).collect(Collectors.toList());
+		return list.stream().map(p -> new OrderDTO(p)).collect(Collectors.toList());	
+	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto) {
+		Order order = new Order(null, 
+				dto.getAddress(), dto.getLatitude(),
+				dto.getLongitude(), Instant.now(),
+				OrderStatus.PENDING);
+		for(ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
 		
+		order = repository.save(order);
+		
+		return new OrderDTO(order);
 	}
 }
