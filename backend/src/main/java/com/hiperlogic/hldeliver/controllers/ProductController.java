f54b0034653c9139ad7d4be53b857a8d09f3f773
lordspy <lordspy@gmail.com>
2021-03-03 23:29:38 -0300

    Endpoint Produto Feito

---
+++ backend/src/main/java/com/hiperlogic/hldeliver/controllers/ProductController.java
@@ -0,0 +1,26 @@
+package com.hiperlogic.hldeliver.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hiperlogic.hldeliver.dto.ProductDTO;
+import com.hiperlogic.hldeliver.services.ProductService;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+
+		@Autowired
+		private ProductService service;
+		 
+		@GetMapping
+		public ResponseEntity<List<ProductDTO>> findAll(){
+			List<ProductDTO> list = service.findAll();
+			return ResponseEntity.ok().body(list);
+		}
+}
